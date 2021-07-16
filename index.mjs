fd1e20960937d62424503a3e22c21d60a7a2fa94
guoxingwang <wb-gxw02154864@alibaba-inc.com>
2021-07-16 15:42:13 +0800

    feat: hah

--- index.mjs
+++ index.mjs
@@ -3,9 +3,10 @@
 await $`git status`
 // chalk.blue('Hello world!')
 await $`git add ./`
-chalk.blue('add success')
+console.log(chalk.blue('add success'))
 let msg = await question('Please input commit -msg:')
-chalk.red(msg)
+console.log(chalk.red(msg))
+
 await $`git commit -m 'feat: ${msg}'`
 await $`git push`
 
