7f88207d377e5e8fa81b387d6d0e471130b26462
guoxingwang <wb-gxw02154864@alibaba-inc.com>
2021-07-20 14:02:29 +0800

    feat: addline

--- index.mjs
+++ index.mjs
@@ -5,6 +5,7 @@ await $`git status`
 await $`git add ./`
 console.log(chalk.blue('add success'))
 let msg = await question('Please input commit -msg:')
-
+console.log(msg);
+msg = msg.toString()
 await $`git commit -m 'feat: ${msg}'`
 await $`git push`
